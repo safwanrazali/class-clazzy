a2a2864f2ad11f68b64b781675328abb8e90f612
digitallola <cloud@lola.my>
2022-10-06 22:05:52 +0800

    switch

--- javascript/day5/readmeday5.js
+++ javascript/day5/readmeday5.js
@@ -42,3 +42,20 @@ do {
   console.log (counter)
 }
 while (counter <10)
+
+
+const expr = 'Mangoes';
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+    console.log('Mangoes are $2.79 a pound.');
+    break;
+  case 'Papayas':
+    console.log('Papayas are $2.79 a pound.');
+    // expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
