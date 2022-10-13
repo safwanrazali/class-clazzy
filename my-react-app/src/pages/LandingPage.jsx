f5294d8ebd44a446a55fafe6613c870eff94d517
safwanrazali <a.safwanrazali@gmail.com>
2022-10-13 22:15:05 +0800

    useEffect done with example

--- my-react-app/src/pages/LandingPage.jsx
+++ my-react-app/src/pages/LandingPage.jsx
@@ -1,8 +1,13 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const LandingPage = () => {
 
     const [firstName, setFirstName] = useState ("-")
+    const [age, setAge] = useState (31)
+    const [job, setJob] = useState ("-")
+    useEffect(() => {
+        console.log ("i run")
+    },[firstName])
 
     return (
     <div>
@@ -11,6 +16,18 @@ const LandingPage = () => {
     </h1>
 
     <input type={"text"} onChange={(evt) => setFirstName(evt.target.value)} />
+
+    <h1>
+    Umur saya: {age}
+    </h1>
+
+    <input type={"text"} onChange={(evt) => setAge(evt.target.value)} />
+
+    <h1>
+    Saya bekerja sebagai: {job}
+    </h1>
+
+    <input type={"text"} onChange={(evt) => setJob(evt.target.value)} />
     </div>
 
 
