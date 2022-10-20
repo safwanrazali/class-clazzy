9a5f200766d7bf7ce04802a68a3bee70ea95b35a
safwanrazali <a.safwanrazali@gmail.com>
2022-10-20 21:40:10 +0800

    1. todo 2. todoList 3. onSave

--- my-react-app/src/pages/LandingPage.jsx
+++ my-react-app/src/pages/LandingPage.jsx
@@ -3,35 +3,34 @@ import MyCard from "../components/MyCard";
 
 const LandingPage = () => {
 
-    const [firstName, setFirstName] = useState ("-")
-    const [age, setAge] = useState (31)
-    const [job, setJob] = useState ("-")
-    useEffect(() => {
-        console.log ("i run")
-    },[firstName])
+const [todo, setTodo] = useState ('')
+const [todoList, setTodoList] = useState ([])
+
+const onSave = () => {
+    if (todo === '') {
+        return
+    }
+
+    //using spread operator to deep clone
+
+    const clonetodoList = [...todoList]
+
+    setTodoList.push(todo)
+
+    setTodoList(clonetodoList)
+
+    setTodo('')
+
+}
 
     return (
     <div>
-    <h1>
-    Nama saya ialah: {firstName}
-    </h1>
 
-    <input type={"text"} onChange={(evt) => setFirstName(evt.target.value)} />
-
-    <h1>
-    Umur saya: {age}
-    </h1>
-
-    <input type={"text"} onChange={(evt) => setAge(evt.target.value)} />
-
-    <h1>
-    Saya bekerja sebagai: {job}
-    </h1>
-
-    <input type={"text"} onChange={(evt) => setJob(evt.target.value)} />
+        <input type="text" value={todo} onChange={evt => setTodo(evt.target.value)} />
+        <button style={{marginLeft:10}} onClick={onSave}>Save</button>
     
-    <MyCard />
-    <MyCard />
+    {/* <MyCard /> */}
+    {/* <MyCard /> */}
 
     </div>
 
